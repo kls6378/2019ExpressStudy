6d0e7ab8a640fa32ca56e215f4550d8ad8db16a7
kls6378 <kls6378@naver.com>
2019-07-16 21:45:02 +0900

    Add CRUD

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var fs = require('fs')
 var template = require('./lib/template.js')
+var path = require('path')
+var sanitizeHtml = require('sanitize-html')
+var qs = require('querystring')
 
 var app = express()
 
@@ -19,8 +22,121 @@ app.get('/', function (req, res) {
   })
 })
 
-app.get('/page', function (req, res) {
-  return res.send('This is page.')
+app.get('/page/:pageId', function (req, res) {
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = req.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update/${sanitizedTitle}">update</a>
+          <form action="/delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      res.send(html)
+    });
+  });
+})
+
+app.get('/create', function(req,res){
+  fs.readdir('./data', function(error, filelist){
+    var title = 'WEB - create';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list, `
+      <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `, '');
+    res.send(html)
+  });
+})
+
+app.post('/create_process',function(req,res){
+  var body = '';
+      req.on('data', function(data){
+          body = body + data;
+      });
+      req.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            res.redirect(`/page/${title}`)
+          })
+      });
+})
+
+app.get('/update/:pageId', function(req,res){
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = req.params.pageId;
+      var list = template.list(filelist);
+      var html = template.HTML(title, list,
+        `
+        <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      );
+      res.send(html)
+    });
+  });
+})
+
+app.post('/update_process', function(req,res){
+  var body = '';
+      req.on('data', function(data){
+          body = body + data;
+      });
+      req.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              res.redirect(`/page/${title}`)
+            })
+          });
+      });
+})
+
+app.post('/delete_process', function(req,res){
+  var body = '';
+      req.on('data', function(data){
+          body = body + data;
+      });
+      req.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var filteredId = path.parse(id).base;
+          fs.unlink(`data/${filteredId}`, function(error){
+            res.redirect('/')
+          })
+      });
 })
 
 app.listen(3000, function () {
